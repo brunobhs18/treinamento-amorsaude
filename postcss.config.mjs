a64b5b09b83b02dbb89919cdb6a56147d86968d7
Bruno Henrique Silva <bruno.silva@ilinksolutions.com.br>
2024-09-22 17:09:05 -0300

    Ajuste para funcionamento Vercel1

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -4,11 +4,7 @@ import autoprefixer from 'autoprefixer';
 const config = {
   plugins: {
     tailwindcss: {},
-<<<<<<< HEAD
     autoprefixer: {}
-=======
-    autoprefixer: {},    
->>>>>>> d5559edd7da9f25327b5d77efde44f5fa935aa33
   },
 };
 
